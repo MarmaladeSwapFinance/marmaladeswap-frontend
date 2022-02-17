6ac724261596f3410f664169af49698f4f231614
Maxim Manylov <manylov@gmail.com>
2022-02-17 10:51:09 +0300

    fix: Replace logos to domain

--- src/components/TokenImage/index.tsx
+++ src/components/TokenImage/index.tsx
@@ -16,7 +16,7 @@ interface TokenPairImageProps extends Omit<UIKitTokenPairImageProps, 'primarySrc
 
 const getImageUrlFromToken = (token: Token) => {
   const address = getAddress(token.symbol === 'BNB' ? tokens.wbnb.address : token.address)
-  if (token.symbol === 'MARM') return 'https://marmaladeswap.surge.sh/logo.png'
+  if (token.symbol === 'MARM') return 'https://marmaladeswap.finance/logo-c.png'
   return `/images/tokens/${address}.png`
 }
 
--- src/utils/getTokenLogoURL.ts
+++ src/utils/getTokenLogoURL.ts
@@ -1,5 +1,5 @@
 const getTokenLogoURL = (address: string) => {
-  if (address === '0x1174ee668CDDBB810eD576eDcD7114513Ea655f6') return 'http://marmaladeswap.surge.sh/logo.png'
+  if (address === '0x1174ee668CDDBB810eD576eDcD7114513Ea655f6') return 'https://marmaladeswap.finance/logo-c.png'
   return `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
 }
 
